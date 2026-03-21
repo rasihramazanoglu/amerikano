d75bf0ceef2b2e713ba025f49a4c5b52b895d00a
rasihramazanoglu <rasih.ramazanoglu@gmail.com>
2026-03-21 20:34:52 +0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,15 @@
 const CACHE = 'amerikano-v1';
 const ASSETS = ['/', '/index.html'];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
 });
